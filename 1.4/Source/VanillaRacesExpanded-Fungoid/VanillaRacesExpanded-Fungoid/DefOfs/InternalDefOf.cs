899b297204902855ec9093944f8edf2be5981f30
SargBjornson <sarg.auxiliar@gmail.com>
2023-07-17 09:02:03 +0200

    Initial commit

---
+++ 1.4/Source/VanillaRacesExpanded-Fungoid/VanillaRacesExpanded-Fungoid/DefOfs/InternalDefOf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VanillaRacesExpandedFungoid
+{
+    [DefOf]
+    public static class InternalDefOf
+    {
+       
+
+
+
+
+    }
+}
