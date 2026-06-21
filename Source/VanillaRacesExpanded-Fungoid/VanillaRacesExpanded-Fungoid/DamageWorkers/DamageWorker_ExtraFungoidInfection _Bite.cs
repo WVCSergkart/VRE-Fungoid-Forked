909702645304d30d982f5d6769e0c25f5219cd79
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2026-06-21 23:22:35 +0300

    infecter upd

--- Source/VanillaRacesExpanded-Fungoid/VanillaRacesExpanded-Fungoid/DamageWorkers/DamageWorker_ExtraFungoidInfection _Bite.cs
+++ Source/VanillaRacesExpanded-Fungoid/VanillaRacesExpanded-Fungoid/DamageWorkers/DamageWorker_ExtraFungoidInfection _Bite.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using RimWorld;
 using Verse;
+using WVC_XenotypesAndGenes;
 
 namespace VanillaRacesExpandedFungoid
 {
@@ -10,73 +11,62 @@ namespace VanillaRacesExpandedFungoid
        
 
         protected override void ApplySpecialEffectsToPart(Pawn pawn, float totalDamage, DamageInfo dinfo, DamageWorker.DamageResult result)
-        {
-            
-            Random random = new Random();
+		{
+			Infector(pawn, dinfo);
+			base.ApplySpecialEffectsToPart(pawn, totalDamage, dinfo, result);
+		}
 
+		public static void Infector(Pawn pawn, DamageInfo dinfo)
+		{
+			if (!pawn.IsHuman())
+			{
+				return;
+			}
+			Pawn attackingPawn = dinfo.Instigator as Pawn;
+			XenotypeDef xenotype = attackingPawn?.genes?.Xenotype ?? null;
+			if (xenotype == null)
+			{
+				return;
+			}
+			if ((pawn.genes?.HasActiveGene(InternalDefOf.VRE_GeneInfector)) != false)
+			{
+				return;
+			}
+			Hediff hediff = pawn?.health?.hediffSet?.GetFirstHediffOfDef(InternalDefOf.VRE_GeneInfection);
+			if (hediff != null || !Rand.Chance(VanillaRacesExpandedFungoid_Settings.infectionChance))
+			{
+				return;
+			}
+			try
+			{
+				pawn.health.AddHediff(InternalDefOf.VRE_GeneInfection, dinfo.HitPart);
+				Hediff hediffApplied = pawn?.health?.hediffSet?.GetFirstHediffOfDef(InternalDefOf.VRE_GeneInfection);
+				hediffApplied.TryGetComp<HediffComp_GeneInfection>()?.xenotypeHolder = new(pawn, false, pawn.genes.Xenogenes.ConvertToDefs());
+				//HediffComp_GeneInfection comp = hediffApplied.TryGetComp<HediffComp_GeneInfection>();
+				//List<GeneDef> xenogenesToPass = new List<GeneDef>();
+				//foreach (Gene xenogene in attackingPawn.genes.Xenogenes)
+				//{
+				//	xenogenesToPass.Add(xenogene.def);
+				//}
+				//comp.xenogenes = xenogenesToPass;
+				//Hediff_GeneInfection hediffWithClass = hediffApplied as Hediff_GeneInfection;
+				//if (attackingPawn.genes.CustomXenotype != null)
+				//{
+				//	comp.xenotypeName = attackingPawn.genes.CustomXenotype.name;
+				//	hediffWithClass.xenotypeName = attackingPawn.genes.CustomXenotype.name;
+				//	comp.iconDef = attackingPawn.genes.CustomXenotype.IconDef;
+				//}
+				//else
+				//{
+				//	comp.xenotypeName = attackingPawn.genes.Xenotype.LabelCap;
+				//	hediffWithClass.xenotypeName = attackingPawn.genes.Xenotype.LabelCap;
+				//	comp.iconDef = attackingPawn.genes.iconDef;
+				//}
+			}
+			catch (Exception)
+			{
+			}
+		}
 
-            if (pawn.RaceProps.Humanlike) {
-
-                Pawn attackingPawn = dinfo.Instigator as Pawn;
-
-                XenotypeDef xenotype = attackingPawn?.genes?.Xenotype ?? null;
-
-                if (xenotype != null)
-                {
-                    if (pawn.genes?.HasActiveGene(InternalDefOf.VRE_GeneInfector) == false)
-                    {
-
-                        Hediff hediff = pawn?.health?.hediffSet?.GetFirstHediffOfDef(InternalDefOf.VRE_GeneInfection);
-
-                        if (hediff == null && random.NextDouble() > (1-VanillaRacesExpandedFungoid_Settings.infectionChance))
-                        {
-                            try
-                            {
-                                pawn.health.AddHediff(InternalDefOf.VRE_GeneInfection,dinfo.HitPart);
-                                Hediff hediffApplied = pawn?.health?.hediffSet?.GetFirstHediffOfDef(InternalDefOf.VRE_GeneInfection);
-                                HediffComp_GeneInfection comp = hediffApplied.TryGetComp<HediffComp_GeneInfection>();
-
-
-                                List<GeneDef> xenogenesToPass = new List<GeneDef>();
-                                foreach (Gene xenogene in attackingPawn.genes.Xenogenes)
-                                {
-                                    xenogenesToPass.Add(xenogene.def);
-                                }
-                                comp.xenogenes = xenogenesToPass;
-                                Hediff_GeneInfection hediffWithClass = hediffApplied as Hediff_GeneInfection;
-                                if (attackingPawn.genes.CustomXenotype != null)
-                                {
-                                    comp.xenotypeName = attackingPawn.genes.CustomXenotype.name;
-                                    hediffWithClass.xenotypeName = attackingPawn.genes.CustomXenotype.name;
-                                    comp.iconDef = attackingPawn.genes.CustomXenotype.IconDef;
-
-                                }
-                                else
-                                {
-                                    comp.xenotypeName = attackingPawn.genes.Xenotype.LabelCap;
-                                    hediffWithClass.xenotypeName = attackingPawn.genes.Xenotype.LabelCap;
-                                    comp.iconDef = attackingPawn.genes.iconDef;
-
-                                }
-
-
-
-                               
-                                
-                            }
-                            catch (Exception) { }
-
-                        }
-
-                    }
-
-                }
-
-            }
-
-            base.ApplySpecialEffectsToPart(pawn, totalDamage, dinfo, result);
-
-
-        }
-    }
+	}
 }
--- Source/VanillaRacesExpanded-Fungoid/VanillaRacesExpanded-Fungoid/HediffComps/HediffComp_GeneInfection.cs
+++ Source/VanillaRacesExpanded-Fungoid/VanillaRacesExpanded-Fungoid/HediffComps/HediffComp_GeneInfection.cs
@@ -5,6 +5,7 @@ using Verse.Noise;
 using System.Collections.Generic;
 using UnityEngine.Analytics;
 using System.Linq;
+using WVC_XenotypesAndGenes;
 
 namespace VanillaRacesExpandedFungoid
 {
@@ -12,9 +13,11 @@ namespace VanillaRacesExpandedFungoid
     {
 
 
-        public List<GeneDef> xenogenes = new List<GeneDef>();
-        public string xenotypeName;
-        public XenotypeIconDef iconDef;
+        //public List<GeneDef> xenogenes = new List<GeneDef>();
+        //public string xenotypeName;
+        //public XenotypeIconDef iconDef;
+
+        public XenotypeHolder_Exposable xenotypeHolder;
 
         public HediffCompProperties_GeneInfection Props
         {
@@ -27,65 +30,56 @@ namespace VanillaRacesExpandedFungoid
         public override void CompExposeData()
         {
             base.CompExposeData();
-
-
-            Scribe_Collections.Look(ref this.xenogenes, nameof(this.xenogenes), LookMode.Def);
-            Scribe_Values.Look(ref this.xenotypeName, nameof(this.xenotypeName));
-            Scribe_Defs.Look(ref this.iconDef, nameof(this.iconDef));
-
-
-        }
+            //Scribe_Collections.Look(ref this.xenogenes, nameof(this.xenogenes), LookMode.Def);
+            //Scribe_Values.Look(ref this.xenotypeName, nameof(this.xenotypeName));
+            //Scribe_Defs.Look(ref this.iconDef, nameof(this.iconDef));
+			Scribe_Deep.Look(ref xenotypeHolder, "xenotypeHolder");
+		}
 
         public override void CompPostTickInterval(ref float severityAdjustment, int delta)
         {
-            base.CompPostTickInterval(ref severityAdjustment, delta);
-
             if (parent.Severity > 0.99f)
             {
-
-                if (parent.pawn.Map != null)
+				Pawn pawn = parent.pawn;
+				if (pawn.Map != null)
                 {
                     for (int i = 0; i < 20; i++)
                     {
                         IntVec3 c;
-                        CellFinder.TryFindRandomReachableCellNearPosition(parent.pawn.Position, parent.pawn.Position, parent.pawn.Map, 2, TraverseParms.For(TraverseMode.NoPassClosedDoors, Danger.Deadly, false), null, null, out c);
+                        CellFinder.TryFindRandomReachableCellNearPosition(pawn.Position, pawn.Position, pawn.Map, 2, TraverseParms.For(TraverseMode.NoPassClosedDoors, Danger.Deadly, false), null, null, out c);
 
-                        FilthMaker.TryMakeFilth(c, parent.pawn.Map, ThingDefOf.Filth_Slime);
+                        FilthMaker.TryMakeFilth(c, pawn.Map, ThingDefOf.Filth_Slime);
                     }
-
-                    InternalDefOf.Hive_Spawn.PlayOneShot(new TargetInfo(parent.pawn.Position, parent.pawn.Map, false));
+                    InternalDefOf.Hive_Spawn.PlayOneShot(new TargetInfo(pawn.Position, pawn.Map, false));
                 }
+                //if (parent.pawn.genes != null)
+                //{
+                //    List<Gene> geneListBackup = (from x in parent.pawn.genes.GenesListForReading
+                //        where x.def.exclusionTags?.Contains("HairStyle")==false
+                //        select x).ToList();
 
-                if (parent.pawn.genes != null)
-                {
-                    List<Gene> geneListBackup = (from x in parent.pawn.genes.GenesListForReading
-                        where x.def.exclusionTags?.Contains("HairStyle")==false
-                        select x).ToList();
+                //    parent.pawn.genes.SetXenotype(XenotypeDefOf.Baseliner);
 
-                    parent.pawn.genes.SetXenotype(XenotypeDefOf.Baseliner);
+                //    foreach (Gene gene in geneListBackup)
+                //    {
+                //        parent.pawn.genes.AddGene(gene.def, false);
 
-                    foreach (Gene gene in geneListBackup)
-                    {
-                        parent.pawn.genes.AddGene(gene.def, false);
-
-                    }
+                //    }
       
-                    parent.pawn.genes.xenotypeName = xenotypeName;
-                    parent.pawn.genes.iconDef = iconDef;
+                //    parent.pawn.genes.xenotypeName = xenotypeName;
+                //    parent.pawn.genes.iconDef = iconDef;
              
-                    foreach (GeneDef geneDef in xenogenes)
-                    {
-                        parent.pawn.genes.AddGene(geneDef, true);
-                    }
-                }
-
-                parent.pawn.health.AddHediff(InternalDefOf.VRE_GeneInfected);
-
-                parent.pawn.health.hediffSet.hediffs.Remove(parent);
-
-
+                //    foreach (GeneDef geneDef in xenogenes)
+                //    {
+                //        parent.pawn.genes.AddGene(geneDef, true);
+                //    }
+                //}
+                ReimplanterUtility.ConvertXenogenesToEndogenes(pawn);
+                ReimplanterUtility.SetXenotype(pawn, xenotypeHolder);
+                pawn.health.AddHediff(InternalDefOf.VRE_GeneInfected);
+                pawn.health.RemoveHediff(parent);
+                //parent.pawn.health.hediffSet.hediffs.Remove(parent);
             }
-
         }
 
         public override IEnumerable<Gizmo> CompGetGizmos()
@@ -105,8 +99,5 @@ namespace VanillaRacesExpandedFungoid
                 yield return command_Action;
             }
         }
-
-
-
     }
 }
